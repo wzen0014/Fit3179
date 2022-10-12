a37887954f70273273a78ea2dc7d2445da36cef4
Weihao Zeng <114278868+wzen0014@users.noreply.github.com>
2022-10-13 01:03:25 +1100

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,9 +1,19 @@
-var vg_1 = "h10.map.json";
+var vg_1 = "map.json";
 vegaEmbed("#map", vg_1).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
 
-var vg_2 = "h10.line.json";
+var vg_2 = "line.json";
 vegaEmbed("#line", vg_2).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "bar.json";
+vegaEmbed("#bar", vg_3).then(function(result) {
+// Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "pie.json";
+vegaEmbed("#pie", vg_4).then(function(result) {
 // Access the Vega view instance(https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
